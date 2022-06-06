081538f717f25855b5af4ea7a797423d94844b4f
I2rys <i2rys@protonmail.com>
2022-06-06 09:58:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# NoSSHProx
+Automatically block IPs that are proxy that made a SSH request to your server.
+
+## Installation
+Github:
+```
+git clone https://github.com/I2rys/nosshprox
+```
+
+NpmJS:
+```
+npm i parallel-park request-async shelljs lodash fs
+```
+
+Packages:
+```
+sudo apt install iptables -y
+```
+
+## Usage
+```
+node index.js
+```
+
+## License
+MIT © I2rys
---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// Dependencies
+const { runJobs } = require("parallel-park")
+const request = require("request-async")
+const shellJS = require("shelljs")
+const _ = require("lodash")
+const fs = require("fs")
+
+// Main
+setInterval(async function(){
+    const blockedIPs = shellJS.exec("sudo iptables --list-rules", { silent: true }).stdout
+
+    var ips = shellJS.exec("sudo cat /var/log/auth.log", { silent: true }).stdout.match(/\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/gim)
+
+    if(ips){
+        ips = _.uniq(ips)
+
+        await runJobs(
+            ips,
+            async(ip, index, max)=>{
+                if(blockedIPs.indexOf(ip) === -1){
+                    var response = await request(`http://167.172.85.80/api/check/proxy?ip=${ip}`)
+                    response = JSON.parse(response.body)
+        
+                    if(response.proxy){
+                        console.log(`Proxy detected: ${ip}`)
+                        shellJS.exec(`sudo iptables -D INPUT -s ${ip} -j DROP`, { silent: true })
+                    }
+                }
+            }
+        )
+
+        shellJS.exec("sudo cp /dev/null /var/log/auth.log", { silent: true })
+    }
+}, 3000)
